55c4954990618322f8d80ec8b5b4573e0997b43f
Namktpmvl <55733541+Namktpmvl@users.noreply.github.com>
2019-10-03 01:01:23 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4_continue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = -145;
+            Console.WriteLine(Absolute(n));
+        }
+
+        public static int Power(int x, int y)
+        {
+            if (y == 0)
+                return 1;
+            else if (y % 2 == 0)
+                return Power(x, y / 2) * Power(x, y / 2);    
+            else
+                return x * Power(x, y / 2) * Power(x, y / 2); 
+        }
+
+        public static int Square(int n)
+        {
+            int a = n;
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return a * a;   
+        }
+
+        public static int Cube(int n1)
+        {
+            int a = n1;
+            if (n1 == 0)
+                return 0;
+            else if (n1 == 1)
+                return 1;
+            else
+                return a * a * a;
+        }
+
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+            return true; 
+        }
+
+        public static int Absolute(int n)
+        {
+            if (n < 0)
+                return n * -1 ;
+            for (int i = 0; i < n; i++)
+                if (n > i)
+                    return n ;
+            return n * -1  ; 
+
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise4_continue;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Power()
+        {
+            int result = Program.Power(2, 3);
+            Assert.AreEqual(8,result);
+        }
+
+        [TestMethod]
+        public void Square()
+        {
+            int result2 = Program.Square(2);
+            Assert.AreEqual(4, result2);
+        }
+
+        [TestMethod]
+        public void Cube()
+        {
+            int result3 = Program.Cube(3);
+            Assert.AreEqual(27, result3);
+        }
+
+        [TestMethod]
+        public void IsPrime()
+        {
+            bool result4 = Program.IsPrime(7);
+            Assert.AreEqual(true, result4);
+        }
+
+        [TestMethod]
+        public void Absolute()
+        {
+            int result5 = Program.Absolute(-149);
+            Assert.AreEqual(149, result5);
+        }
+    }
+}
